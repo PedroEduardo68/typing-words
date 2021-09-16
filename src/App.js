84f1a75ec92140e42e4befa0a198e0d04b6d20a9
Pedro <pedro.eduardo.camera@gmail.com>
2021-09-16 00:39:10 -0300

    commit 16 9 21 noite

--- src/App.js
+++ src/App.js
@@ -1,13 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import wordList from "./resources/words.json"
+
+
+
+const MAX_TYPED_KEYS = 30;
+
+const getWordFile = () =>{
+  const index = Math.floor(Math.random()* wordList.length);
+  const word = wordList[index];
+  return word.toLowerCase();
+}
+
+const isValidKey = (key, word) =>{
+  if(!word) return false;
+  const result = word.split('').includes(key);
+  return result;
+}
+
+const Word =({word,validKeys})  =>{
+  if(!word) return null;
+  const joinedKeys = validKeys.join('')
+  const matched = word.slice(0, joinedKeys.length);
+  const remainder = word.slice(joinedKeys.length);
+
+  return (<>
+    <span className="matched">{matched}</span>
+    <span className="remainder">{remainder}</span> 
+  </>)
+}
 
 const App = () => {
+
+  //console.log('word', getWordFile());
+
+  const [getTypedKeys, setTypedKeys] = useState([]);
+  const [getValidKeys, setValidKeys] = useState([]);
+  const [getWord, setWord] = useState('');
+
+  useEffect(()=>{
+    setWord(getWordFile());
+  }, [])
+
+
+  const handleKeyDown = (e) =>{
+    e.preventDefault();
+    const { key } = e;
+    setTypedKeys((prevTypedKeys =>{
+      return [...prevTypedKeys, key].slice(MAX_TYPED_KEYS*-1);
+    }))
+
+    if(isValidKey(key, getWord)){
+      setValidKeys((prev)=>{
+        const isValidLength = prev.length <= getWord.length;
+        const isNextChar = isValidLength && getWord[prev.length] === key;
+
+        console.log(prev, prev.length);
+        console.log(getWord);
+        console.log(isNextChar);
+
+
+        return (isNextChar) ? [...prev, key] : prev;
+      })
+    }
+
+    //console.log("key", key)
+  }
+
   return (
-  <div className="container">
+  <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
     <div className="valid-keys">
-      <span className="matched">tes</span>
-      <span className="remainder">te</span>
+
+      <Word word={getWord} validKeys={getValidKeys} />
+
+      {
+      //<span className="matched">tes</span>
+      //<span className="remainder">te</span> 
+      }
+
     </div>
-    <div className="typed-keys"></div>
+    <div className="typed-keys">{getTypedKeys}</div>
     <div className="completed-words">
       <ol>
         <li>teste</li>
